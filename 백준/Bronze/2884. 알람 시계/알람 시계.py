fefa92ad52197e774c89e7201b5c906d23b1b24c
NoTaste <80740691+NoTaste-def@users.noreply.github.com>
2024-01-19 01:48:32 +0900

    Update 알람 시계.py
    수학을 이용한 방법 추가.

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,3 +1,11 @@
+# h, m = map(int, input().split())
+# total = h * 60 + m - 45
+# if total < 0:
+#     total += (60 * 24) # 예외처리.
+# print(total // 60, total % 60)
+# # 전체를 60으로 나눈 몫은 시간.
+# # 전체를 60으로 나눈 나머지는 분.
+
 a, b = map(int, input().split())
 
 if b < 45 and a > 0:
