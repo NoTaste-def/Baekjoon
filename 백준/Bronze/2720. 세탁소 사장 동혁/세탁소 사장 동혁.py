5dcad026640b09b60761e92f2b25238b67a058ec
NoTaste <80740691+NoTaste-def@users.noreply.github.com>
2024-02-03 01:10:58 +0900

    Update 세탁소 사장 동혁.py
    Solved by Recursion

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -3,28 +3,28 @@ T = int(input())
 result = []
 a = [25, 10, 5, 1]
 
-# def remain(money, coin, i=0):
-#   if len(result) == 4:
-#     return print(*result)
-#   quotient, remainder = divmod(money,coin[i])
-#   result.append(quotient)
-#   i += 1
-#   return  remain(remainder, a[i], i)
+def remain(money, coin, i):
+  if len(result) == 4:
+    return print(*result)
+  quotient, remainder = divmod(money,coin[i])
+  result.append(quotient)
+  return  remain(remainder, coin, i+1)
 
-# for _ in range(T):
-#   c = int(input())
-#   remain(c, a)
-
-for i in range(T):
+for _ in range(T):
   c = int(input())
-  quotientQ, remainderQ = divmod(c,25)
-  result.append(quotientQ)
-  quotientD, remainderD = divmod(remainderQ,10)
-  result.append(quotientD)
-  quotientN, remainderN = divmod(remainderD,5)
-  result.append(quotientN)
-  result.append(remainderN)
-  print(*result)
+  remain(c, a, 0)
   result=[]
+
+# for i in range(T):
+#   c = int(input())
+#   quotientQ, remainderQ = divmod(c,25)
+#   result.append(quotientQ)
+#   quotientD, remainderD = divmod(remainderQ,10)
+#   result.append(quotientD)
+#   quotientN, remainderN = divmod(remainderD,5)
+#   result.append(quotientN)
+#   result.append(remainderN)
+#   print(*result)
+#   result=[]
   
   
